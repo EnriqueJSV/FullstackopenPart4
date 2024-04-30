e3e5d465162af3be8f7ba202a1873a7bb9864367
Enrique <ejsv1899@gmail.com>
2024-04-30 11:45:28 -0600

    ejercicio 4.8 completado

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
 
-let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+let PORT = process.env.PORT;
+
+let MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 module.exports = {
-    PORT,
-    MONGODB_URI
-}
+  PORT,
+  MONGODB_URI,
+};
