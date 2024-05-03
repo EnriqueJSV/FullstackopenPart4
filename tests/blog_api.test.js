019cc767c279a05502c79534ef10428648713972
Enrique <ejsv1899@gmail.com>
2024-05-02 19:40:34 -0600

    ejercicio 4.12 completado

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -74,6 +74,34 @@ test("if likes is not added then return 0", async () => {
   assert.strictEqual(lastBlog.likes, 0);
 });
 
+test("blog without title is not added", async () => {
+  const newBlog = {
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+  };
+
+  await api.post("/api/blogs").send(newBlog).expect(400);
+
+  const response = await api.get("/api/blogs");
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length);
+});
+
+test("blog without URL is not added", async () => {
+  const newBlog = {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    likes: 10,
+  };
+
+  await api.post("/api/blogs").send(newBlog).expect(400);
+
+  const response = await api.get("/api/blogs");
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length);
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
